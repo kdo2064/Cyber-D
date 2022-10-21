0937cf91971189e642f5860f63185249feb3c49b
KDO <114388910+kdo2064@users.noreply.github.com>
2022-10-21 21:07:27 +0545

    Add files via upload

---
+++ CyberD.py
@@ -0,0 +1,75 @@
+from termcolor import colored
+import os
+#import requests
+from logo import logo
+from logo import social_media
+from logo import tools
+from function import update
+from function import contact
+from function import exit
+
+# import function
+
+
+from function import zphisher
+from function import Red_Hawk
+from function import ngrok
+from function import MaskPhish
+from function import infect
+from function import AndroRat
+from function import IPTracker
+from function import PyPhisher
+from function import kalimux
+from function import PhoneInfoga
+from function import T_Bomber
+from function import metasploit
+from function import mrphish
+from function import IpHack
+from function import tunnel
+from function import Slowloris
+
+os.system("clear")
+print(colored(logo,'blue') + colored(social_media,'red') + colored(tools,'green'))
+
+
+choose=input(colored("   Choose a Option:",'yellow'))
+
+if choose == "1":
+    zphisher()
+    
+elif choose == "2":
+    Red_Hawk()
+elif choose == "3":
+    ngrok()
+elif choose == "4":
+    MaskPhish()
+elif choose == "5":
+    infect()
+elif choose == "6":
+    AndroRat()
+elif choose == "7":
+    IPTracker()
+elif choose == "8":
+    PyPhisher()
+elif choose == "9":
+    kalimux()
+elif choose == "10":
+    PhoneInfoga()
+elif choose == "11":
+    T_Bomber()
+elif choose == "12":
+    metasploit()
+elif choose == "13":
+    mrphish()
+elif choose == "14":
+    IpHack()
+elif choose== "15":
+    tunnel()
+elif choose== "16":
+    Slowloris()
+elif choose == "0":
+    update()
+elif choose == "99":
+    contact()
+elif choose== "999":
+    exit()
---
+++ logo.py
@@ -0,0 +1,46 @@
+logo= """
+
+
+  ██████╗██╗   ██╗██████╗ ███████╗██████╗       ██████╗ 
+ ██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗      ██╔══██╗
+ ██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝█████╗██║  ██║
+ ██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗╚════╝██║  ██║
+ ╚██████╗   ██║   ██████╔╝███████╗██║  ██║      ██████╔╝
+  ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝      ╚═════╝ 
+                                                      v1.0                                           
+                                                                                    
+ """
+
+social_media= """
+ ~ Instagram: https://instagram.com/kdo_shashank
+ ~ Whatsapp: https://wa.me/+9779746554757
+ ~ Telegram: https://t.me/kdobhai
+"""
+tools="""
+   {1}--Zphisher
+   {2}--Red Hawk
+   {3}--ngrok
+   {4}--MaskPhish
+   {5}--infect
+   {6}--AndroRat
+   {7}--IP-Tracer
+   {8}--PyPhisher
+   {9}--kalimux
+   {10}--PhoneInfoga
+   {11}--T-Bomber
+   {12}--metasploit
+   {13}--mrphish
+   {14}--IpHack
+   {15}--tunnel
+   {16}--Slowloris(D-Dos)
+   {0}--Update
+   {99}--Contact
+   {999}--exit
+ """
+
+cont="""
+   {1}--Whatsapp
+   {2}--Telegram
+   {3}--Instagram
+   {4}--Youtube
+"""
